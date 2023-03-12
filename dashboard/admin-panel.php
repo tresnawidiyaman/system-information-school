e7161aa5992af622c7290df14bcf61f87ae877c3
tresnawidiyaman <tresna.widiyaman@gmail.com>
2023-03-12 21:46:41 +0700

    perubahan judul

--- dashboard/admin-panel.php
+++ dashboard/admin-panel.php
@@ -8,20 +8,20 @@ if (! defined( 'ABSPATH' )) {
 function dashboard_admin () {
      
      add_menu_page(
-        'LMS Sekolah', //Tittle Web Plugin
-        'LMS Sekolah', //Tittle Menu plugin
+        'Sysinfo School', //Tittle Web Plugin
+        'Sysinfo School', //Tittle Menu plugin
         '', //Kapabilitas menu, ini bisa diakses oleh siapa (disini diakses oleh admin)
-        'lmspage', //Slug menu (url) dari apps di adminnya
+        'sispage', //Slug menu (url) dari apps di adminnya
         'dashboardcc_function', //fungsi diarahkan ke submenu
         'dashicons-welcome-learn-more' //Icon yang akan digunakan sc : https://developer.wordpress.org/resource/dashicons/#chart-line
     );
     add_submenu_page(
-        'lmspage', //Judul Slug menu utama (url)
-        'Dashboard LMS Sekolah', //Tittle Web Plugin
+        'sispage', //Judul Slug menu utama (url)
+        'Sysinfo School', //Tittle Web Plugin
         'Dashboard', //Tittle Menu plugin
-        'manage_options', //Kapabilitas menu, ini bisa diakses oleh siapa (disini diakses oleh admin)
-        'dashboardlms', //url dari apps di adminnya
-        'dashboardlms_function' //fungsi untuk menjalankan menu, jadi kalau menu di klik maka fungsi apa yang jalan
+        'manage_options', //Kapabccilitas menu, ini bisa diakses oleh siapa (disini diakses oleh admin)
+        'dashboardsis', //url dari apps di adminnya
+        'dashboardsis_function' //fungsi untuk menjalankan menu, jadi kalau menu di klik maka fungsi apa yang jalan
     );
     
 
